810b677687225a6d10082789a949f2c46c1bc898
AjayPeter865 <ajaypeter865@gmail.com>
2025-09-25 14:07:05 +0530

    Implimented toastify

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 const path = require('path')
 const PORT = process.env.PORT || 8000;
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
+
 
 // IMPORT MODULES
 const { connectMongoDB } = require('./connect');
@@ -16,13 +20,19 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
-
+app.use(session({
+    secret: process.env.sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+}))
+app.use(flash())
 
 
 // SET EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
 // ROUTES
 app.use('/', staticRoutes)
