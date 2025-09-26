b502aac2f682cbcb45536c1a79ae789eeb22f6b4
AjayPeter865 <ajaypeter865@gmail.com>
2025-09-26 14:24:45 +0530

    Implimenting passport

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,11 @@
 const passport = require('passport')
+require('dotenv').config()
 
 const GoogleStratergy = require('passport-google-oauth20')
 
 passport.use(new GoogleStratergy({
-    
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+
 }))
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,17 @@
+// IMPORT MIDDLEWARES
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+
 
 // CONTROLLERS
-
 const { signupUser, loginUser } = require('../controllers/user/authController')
 const { signUpValidator, loginValidator } = require('../middlewares/validation')
 
 
 router.post('/signup', signUpValidator, signupUser)
 router.post('/login', loginValidator, loginUser)
+router.get('/auth/google/callback', passport.Authenticator)
 
 
 // 
